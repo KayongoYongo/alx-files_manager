dc3083c8edc3907ec31d77a3175da102c9f09595
KayongoYongo <sknsammie10@gmail.com>
2023-09-04 17:18:06 +0000

    redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -11,32 +11,32 @@ class RedisClient {
     });
   }
 
-// check connection status and report
-isAlive() {
-  if (this.client.connected) {
-    return true;
+  // check connection status and report
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
   }
-  return false;
-}
 
-// get value for given key from redis server
-async get(key) {
-  const redisGet = promisify(this.client.get).bind(this.client);
-  const value = await redisGet(key);
-  return value;
-}
+  // get value for given key from redis server
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
+  }
 
-// set key value pair to redis server
-async set(key, value, time) {
-  const redisSet = promisify(this.client.set).bind(this.client);
-  await redisSet(key, value);
-  await this.client.expire(key, time);
-}
+  // set key value pair to redis server
+  async set(key, value, time) {
+    const redisSet = promisify(this.client.set).bind(this.client);
+    await redisSet(key, value);
+    await this.client.expire(key, time);
+  }
 
-// del key vale pair from redis server
-async del(key) {
-  const redisDel = promisify(this.client.del).bind(this.client);
-  await redisDel(key);
+  // del key vale pair from redis server
+  async del(key) {
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
   }
 }
 
